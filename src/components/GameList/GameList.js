33b9b4cabec9b5c92817a39fd8b8e1363188d8d7
Sergei Piadyshev <s.pyadyshev@gmail.com>
2018-10-22 09:04:10 +0300

    test

--- src/components/GameList/GameList.js
+++ src/components/GameList/GameList.js
@@ -4,21 +4,14 @@ import { connect } from 'react-redux';
 import './GameList.scss';
 
 class GameList extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            games: []
-        }
-    }
 
     componentDidMount() {
         const url = "https://raw.githubusercontent.com/s-pyadyshev/beat-em-ups-api/master/beatemups.json";
         fetch(url)
             .then(response => response.json())
             .then(data => {
-                this.setState({
-                    games: data
-                });
+                console.log(data);
+                return data;
             }
         )
     }
@@ -26,25 +19,30 @@ class GameList extends React.Component {
     render() { 
         return (
             <ul className="game-list">
-                {this.state.games.map((item, index) =>
+                {/* {this.props.games.map((item, index) =>
                     <li className="game-list__item"
                         key={index}>
                         <GameListItem
                             name={item.name}
                         />
                     </li>
-                )}
+                )} */}
             </ul>
         );
     }
 }
 
-const mapStateToProps = store => {
-    console.log(store);
+const mapStateToProps = state => {
     return {
-        games: store.games,
+      games: state
     }
 }
+
+// const mapDispatchToProps = dispatch => {
+//     return {
+//       submitNewMessage: (message) => dispatch(addMessage(message))
+//     }
+// }
 
 // export default GameList;
 export default connect(mapStateToProps)(GameList);
--- src/components/apiClient.js
+++ src/components/apiClient.js
@@ -1,9 +1,13 @@
-const apiClient = {
-    get(url) {
-        return fetch(url)
-            .then(response => response.json())
-            .then(data => data)
-    }
+const apiClient = () => {
+    const url = "https://raw.githubusercontent.com/s-pyadyshev/beat-em-ups-api/master/beatemups.json";
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            this.setState({
+                games: data
+            });
+        }
+    )
 };
 
 export default apiClient;
