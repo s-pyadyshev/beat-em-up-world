f54f967356f7c7768bc51f6ffc5c81a0688f7b69
sergey pyadishev <sergey.pyadishev@simbirsoft.com>
2018-12-07 15:35:36 +0300

    update

--- src/components/GameListContainer/index.js
+++ src/components/GameListContainer/index.js
@@ -7,21 +7,32 @@ class GameList extends React.Component {
     super(props);
     this.state = {
       games: [],
-      filteredGames: [],
+      isLoading: false,
+      error: null
+      // filteredGames: [],
       // players: 1
     };
     // this.filterByPlayers = this.filterByPlayers.bind(this);
   }
 
-  componentDidMount() {
-    // const URL = "https://raw.githubusercontent.com/s-pyadyshev/beat-em-ups-api/master/beatemups.json";
-    // fetch(URL)
-    //   .then(response => response.json())
-    //   .then(json => this.setState({
-    //     games: json,
-    //     filteredGames: json
-    //   }))
-  }
+  // the most basic React.js fetch API example
+  // componentDidMount() {
+  //   this.setState({ isLoading: true });
+  //   const URL = "https://raw.githubusercontent.com/s-pyadyshev/beat-em-ups-api/master/beatemups.json";
+  //   fetch(URL)
+      // .then(response => {
+      //   if (response.ok) {
+      //     return response.json();
+      //   } else {
+      //     throw new Error('Something went wrong ...');
+      //   }
+      // })
+  //     .then(json => this.setState({
+  //       games: json,
+  //       isLoading: false
+  //     }))
+  //     .catch(error => this.setState({ error, isLoading: false }));
+  // }
 
   // filterByPlayers(event) {
   //   this.setState({
@@ -30,25 +41,34 @@ class GameList extends React.Component {
   //   });
   // }
 
-    render() {
-        return (
-          <div>
-            <ul className="game-list">
-                {this.state.filteredGames.map((item, index) => {
-                    return (
-                      <li
-                        className="game-list__item"
-                        key={index}>
-                        <GameListItem
-                            name={item.name}
-                        />
-                      </li>
-                    )}
-                )}
-            </ul>
-          </div>
-        );
+  render() {
+    const { games, isLoading, error } = this.state;
+
+    if (error) {
+      return <p>{error.message}</p>;
     }
+
+    if (isLoading) {
+      return <p>Loading ...</p>;
+    }
+    return (
+      <div>
+        <ul className="game-list">
+          {games.map((item, index) => {
+            return (
+              <li
+                className="game-list__item"
+                key={index}>
+                <GameListItem
+                    name={item.name}
+                />
+              </li>
+            )}
+          )}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default GameList;
--- src/components/apiClient.js
+++ src/components/apiClient.js
@@ -1,23 +1,23 @@
-import { fetchPostsRequest, fetchPostsSuccess, fetchPostsError } from './../actions';
+// import { fetchPostsRequest, fetchPostsSuccess, fetchPostsError } from './../actions';
 
-function fetchPosts() {
-    const URL = "https://raw.githubusercontent.com/s-pyadyshev/beat-em-ups-api/master/beatemups.json";
-    return fetch(URL, { method: 'GET'})
-       .then( response => Promise.all([response, response.json()]));
-  }
+// function fetchPosts() {
+//     const URL = "https://raw.githubusercontent.com/s-pyadyshev/beat-em-ups-api/master/beatemups.json";
+//     return fetch(URL, { method: 'GET'})
+//        .then( response => Promise.all([response, response.json()]));
+//   }
 
-const apiClient = () => {
-    return (dispatch) => {
-        dispatch(fetchPostsRequest());
-        return fetchPosts().then(([response, json]) =>{
-          if(response.status === 200){
-            dispatch(fetchPostsSuccess(json))
-          }
-          else{
-            dispatch(fetchPostsError())
-          }
-        })
-      }
-};
+// const apiClient = () => {
+//     return (dispatch) => {
+//         dispatch(fetchPostsRequest());
+//         return fetchPosts().then(([response, json]) =>{
+//           if(response.status === 200){
+//             dispatch(fetchPostsSuccess(json))
+//           }
+//           else{
+//             dispatch(fetchPostsError())
+//           }
+//         })
+//       }
+// };
 
-export default apiClient;
+// export default apiClient;
