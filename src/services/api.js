5cffc7788fe84b9c1035c87b4108995a111d4682
admin <admin@MacBook-Pro-admin.local>
2020-03-25 00:43:44 +0400

    hotfix

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,3 @@
-import axios from "axios";
-
 const URL = "https://raw.githubusercontent.com/s-pyadyshev/beat-em-ups-api/master/db.json";
 
 export const Games = {
@@ -8,7 +6,6 @@ export const Games = {
       .then(res => res.json())
       // .then(data => data)
       // .catch(err => console.error(err));
-    // return axios.get(URL);
   },
   getById: () => {
 
