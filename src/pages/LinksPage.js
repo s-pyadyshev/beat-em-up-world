e2a534f9503df978b3b7dc2826e306532655cf6b
s.piadyshev <s.pyadyshev@gmail.com>
2020-04-05 00:58:44 +0300

    update links

--- src/pages/LinksPage.js
+++ src/pages/LinksPage.js
@@ -9,25 +9,28 @@ const LinksPage = () => {
           <a href="http://www.punchpedia.com" target="_blank" rel="noopener noreferrer">PunchPedia</a>
         </li>
         <li>
-          <a href="https://en.wikipedia.org/wiki/Beat_'em_up" target="_blank" rel="noopener noreferrer">Beat_'em_up on Wikipedia</a>
+          <a href="https://en.wikipedia.org/wiki/Beat_'em_up" target="_blank" rel="noopener noreferrer">Beat'em ups on Wikipedia</a>
         </li>
         <li>
-          <a href="https://www.spriters-resource.com/genre/beat_em_up/" target="_blank" rel="noopener noreferrer">Beat_'em_up on The Spriters Resource</a>
+          <a href="https://www.mobygames.com/genre/sheet/beatemup-brawler/" target="_blank" rel="noopener noreferrer">Beat'em ups on MobyGames</a>
         </li>
         <li>
-          <a href="https://store.steampowered.com/tags/en/Beat%20%27em%20up/" target="_blank" rel="noopener noreferrer">Beat_'em_ups on Steam</a>
+          <a href="https://store.steampowered.com/tags/en/Beat%20%27em%20up/" target="_blank" rel="noopener noreferrer">Beat'em ups on Steam</a>
         </li>
         <li>
-          <a href="https://tvtropes.org/pmwiki/pmwiki.php/Main/BeatEmUp/" target="_blank" rel="noopener noreferrer">Beat_'em_ups on TV Tropes</a>
+          <a href="https://tvtropes.org/pmwiki/pmwiki.php/Main/BeatEmUp/" target="_blank" rel="noopener noreferrer">Beat'em ups on TV Tropes</a>
         </li>
         <li>
-          <a href="https://crappygames.miraheze.org/wiki/Category:Beat%27em_Up_Games" target="_blank" rel="noopener noreferrer">Beat_'em_ups on Crappy Games Wiki</a>
+          <a href="https://crappygames.miraheze.org/wiki/Category:Beat%27em_Up_Games" target="_blank" rel="noopener noreferrer">Beat'em ups on Crappy Games Wiki</a>
         </li>
         <li>
-          <a href="https://awesomegames.miraheze.org/wiki/Category:Beat%27em_up_Games" target="_blank" rel="noopener noreferrer">Beat_'em_ups on Awesome Games Wiki</a>
+          <a href="https://awesomegames.miraheze.org/wiki/Category:Beat%27em_up_Games" target="_blank" rel="noopener noreferrer">Beat'em ups on Awesome Games Wiki</a>
         </li>
         <li>
           <a href="https://stayskeptic.wordpress.com/2017/04/22/what-makes-a-beat-em-up" target="_blank" rel="noopener noreferrer">https://stayskeptic.wordpress.com/2017/04/22/what-makes-a-beat-em-up</a>
+        </li>
+        <li>
+          <a href="https://www.spriters-resource.com/genre/beat_em_up/" target="_blank" rel="noopener noreferrer">Beat'em ups on The Spriters Resource</a>
         </li>
         <li>
           <a href="https://sorr.forumotion.net" target="_blank" rel="noopener noreferrer">Streets of Rage Remake Forum</a>
