2c350537b5242ec020f07aa308d2fb3bad8f279a
Sergei Piadyshev <s.pyadyshev@gmail.com>
2021-03-28 19:23:04 +0400

    fix test

--- src/pages/ChartsPage.tsx
+++ src/pages/ChartsPage.tsx
@@ -1,46 +1,45 @@
 import React from "react";
-import {
-  LineChart,
-  Line,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-} from "recharts";
-import { useSelector } from "react-redux";
-import { groupBy } from "lodash";
+// import {
+//   LineChart,
+//   Line,
+//   XAxis,
+//   YAxis,
+//   CartesianGrid,
+//   Tooltip,
+//   Legend,
+// } from "recharts";
+// import { useSelector } from "react-redux";
+// import { groupBy } from "lodash";
 
 export default function ChartsPage() {
-  const gamesList = useSelector((state: any) =>
-    groupBy(state.gamesList.gamesList, (game: any) => game.players)
-  );
-  console.log(gamesList);
-
-  return (
-    <LineChart
-      width={1000}
-      height={500}
-      // data={gamesList}
-      margin={{
-        top: 5,
-        right: 30,
-        left: 20,
-        bottom: 5,
-      }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      <Line
-        type="monotone"
-        dataKey="pv"
-        stroke="#8884d8"
-        activeDot={{ r: 8 }}
-      />
-      <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-    </LineChart>
-  );
+  //   const gamesList = useSelector((state: any) =>
+  //     groupBy(state.gamesList.gamesList, (game: any) => game.players)
+  //   );
+  //   console.log(gamesList);
+  //   return (
+  //     <LineChart
+  //       width={1000}
+  //       height={500}
+  //       // data={gamesList}
+  //       margin={{
+  //         top: 5,
+  //         right: 30,
+  //         left: 20,
+  //         bottom: 5,
+  //       }}
+  //     >
+  //       <CartesianGrid strokeDasharray="3 3" />
+  //       <XAxis dataKey="name" />
+  //       <YAxis />
+  //       <Tooltip />
+  //       <Legend />
+  //       <Line
+  //         type="monotone"
+  //         dataKey="pv"
+  //         stroke="#8884d8"
+  //         activeDot={{ r: 8 }}
+  //       />
+  //       <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+  //     </LineChart>
+  //   );
 }
